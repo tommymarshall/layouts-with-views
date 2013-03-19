3ea3800a1879beb7cadad2722fc1e74be03b8a2f
Tommy Marshall <tommy.marshall@viget.com>
2013-03-19 16:47:02 -0400

    Updated version to 0.3.1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Layouts with Views - A simple layout and view rendering engine built on PHP.
  *
  * @package  Layouts with Views
- * @version  0.3.0
+ * @version  0.3.1
  * @author   Tommy Marshall <tommy.marshall@viget.com>
  * @link     http://viget.com
  */
